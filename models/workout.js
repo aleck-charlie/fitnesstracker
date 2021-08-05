90b62f05354a4f0d067240f2ccac48ad653f5c99
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-05 11:29:46 -0500

    intial folder structure

---
+++ models/workout.js
@@ -0,0 +1,18 @@
+// ne wmongoose schema (copy pasta from 10-16)
+
+// day : date
+
+exercises: [
+    {
+        type: {
+            type: String,
+            trim: true
+        },
+        name: {},
+        weight: {},
+        sets: {},
+        reps: {},
+        duration: {}, 
+        distance: {},
+    }
+]
---
+++ server.js
@@ -0,0 +1,9 @@
+// view
+// router.get('/exercise/')
+// router.get('/stats')
+
+// api
+// router.post('/api/workouts') workout.create({})
+// router.post('/api/workouts/:id') pushing exercise into array
+// router.get('/api/workouts) get all workouts
+// router.get('/api/workouts/range') last 7 combined weights, last 7 combined duration
