ca69cd3732087f6d99ff03cdc33fd3595e32e7b5
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-10 22:15:58 -0500

    completed workout model

--- models/workout.js
+++ models/workout.js
@@ -1,20 +1,40 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const workoutSchema = new Schema ({
-    day: { type: Date, default: () => new Date() },
-
+    day: { 
+        type: Date, 
+        default: () => new Date() 
+    },
 exercises: [
     {
         type: {
             type: String,
-            trim: true
+            trim: true,
+            required: "Exercise required"
         },
-        name: {},
-        weight: {},
-        sets: {},
-        reps: {},
-        duration: {}, 
-        distance: {},
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        }, 
+        distance: {
+            type: Number
+        },
     },
 ],
 });
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
